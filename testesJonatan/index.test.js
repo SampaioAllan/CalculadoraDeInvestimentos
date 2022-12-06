2b73873228c6035d531bf74d187fe3f7ccd67aa1
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-12-05 23:37:31 -0300

    Implementando ajustes

--- testesJonatan/index.test.js
+++ testesJonatan/index.test.js
@@ -1,17 +1,17 @@
 const index = require("../funcoesAllan/index");
 
 test('Calcular a rentabilidade', () => {
-    expect(index.calcularRentabilidade(1000, 2, 1).toBe(240));
+    expect(index.calcularRentabilidade(1000, 2, 1)).toBe("240.00");
 });
 
 test('Calcular a rentabilidade composta', () => {
-    expect(index.calcularRentabilidadeComposta(1000, 2, 1).toBe(240.24));
+    expect(index.calcularRentabilidadeComposta(1000, 2, 1)).toBe("268.24");
 });
 
 test('Calcular a rentabilidade real', () => {
-    expect(index.calcularRentabilidadeReal(8, 4).toBe(3.8));
+    expect(index.calcularRentabilidadeReal(8, 4)).toBe("3.8");
 });
 
 test('Calcular a rentabilidade passada', () => {
-    expect(index.calcularRentabilidadePassada(14, 9).toBe(55.55));
+    expect(index.calcularRentabilidadePassada(14, 9)).toBe("55.56");
 });
